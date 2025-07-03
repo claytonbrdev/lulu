461dc3c469a1bf8c835aa4e48a28a387b29c56a6
CLAYTON SOUZA <magnetikidj@gmail.com>
2025-07-03 03:01:15 -0300

    Added package-lock.json

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: '.',
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
+})
